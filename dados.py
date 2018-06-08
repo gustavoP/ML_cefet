105c055b42a8c074471e81377bb1ef6f4643890a
gustavoP <gustavo.p.epi@gmail.com>
2018-06-08 11:39:13 -0300

    GD

--- dados.py
+++ dados.py
@@ -4,6 +4,18 @@ import matplotlib
 import matplotlib.pyplot as plt
 import static_methods as sm
 
+"""
+All vector must be used in 2 dimensions as in np.array([[1, 2, 3]])
+Not in 1 dimension as in np.array([1, 2, 3])
+"""
+
 a = pd.read_csv('ex1data1.csv', header=None)
 a.plot.scatter(x=0,y=1)
-plt.show()
+#plt.show()
+
+#variables for test
+x = np.array([[1,2],[1,1],[1,2]])
+y = np.array([[0.1],[0.2],[0.4]])
+theta = np.array([[1],[0.6]])
+
+print(sm.GD(X=x,y=y,theta =theta,alfa = 0.1))
--- static_methods.py
+++ static_methods.py
@@ -1,18 +1,47 @@
 import numpy as np
 
-def compute_cost(X, y, theta):
+def compute_cost(X, y, theta, h=None):
     """ 
     X is (m,n) vector.
     y is (m,1)
     theta is (n,1) 
 
+    optional:
+    h = X@theta, if 'h' not numpy.ndarray it won't be used
+
     Return scalar
     """
-    #h = theta'*x
-    #j = 1/m sum((h(i)-y(i))^2 )
-    #a=X*theta-y;
-    #J = 1/(2*m)*(a)'*a;
+
+    m = float(len(y[0]))
+
+    if (h is not None) and (type(h) is np.ndarray):
+        if h.shape != y.shape :
+            raise TypeError("'h' has to be the same dimension as 'y' ")
+        else:
+            a = h-y 
+            
+    else:
+        a = X@theta-y 
     
-    a = X@theta-y 
-    j = 1/(2*len(y))*(a.T@a)
+    j = 1/(2*m)*(a.T@a)
     return j[0,0]
+
+def GD(X, y, theta, alfa, max_iter=10, tolerance=0.1, use_compute_cost = True):
+    """
+    X is (m,n) vector.
+    y is (m,1)
+    theta is (n,1) 
+
+    return theta(n,1), J[iter]
+    """
+    m = float(len(y[0]))
+    if use_compute_cost: J = []
+     
+    for i in range(max_iter):
+        h = X@theta
+        if use_compute_cost: J.append(compute_cost(X,y,theta,h))
+              
+        theta -= (alfa/m)*X.T@(h-y)
+
+    if use_compute_cost: return theta,J
+    else: return theta
