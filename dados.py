4ba708fa2ec0f82d4722ea5645a233954b93a1f4
gustavoP <gustavo.p.epi@gmail.com>
2018-06-13 21:56:25 -0300

    Complete gradiant descent, compute cost, and specific plot function

--- dados.py
+++ dados.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import static_methods as sm
 
 """
@@ -9,13 +7,14 @@ All vector must be used in 2 dimensions as in np.array([[1, 2, 3]])
 Not in 1 dimension as in np.array([1, 2, 3])
 """
 
-a = pd.read_csv('ex1data1.csv', header=None)
-a.plot.scatter(x=0,y=1)
-#plt.show()
+data = pd.read_csv('ex1data1.csv', header=None)
 
-#variables for test
-x = np.array([[1,2],[1,1],[1,2]])
-y = np.array([[0.1],[0.2],[0.4]])
-theta = np.array([[1],[0.6]])
+data.plot.scatter(x=0,y=1, label='Raw Data')
+data_array = data.values
 
-print(sm.GD(X=x,y=y,theta =theta,alfa = 0.1))
+x = np.concatenate((np.ones((len(data_array[:,[0]]),1)),data_array[:,[0]]),axis=1) #data_array[:,0] returns a 1 dimensional array
+y = data_array[:,[1]]
+
+(theta, J)=sm.GD(X=x,y=y,theta =np.array([[0.0],[0.0]]),alfa = 0.001, max_iter=100)
+
+sm.plot(x=x,J=J,theta=theta)
--- static_methods.py
+++ static_methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def compute_cost(X, y, theta, h=None):
     """ 
@@ -10,9 +11,9 @@ def compute_cost(X, y, theta, h=None):
     h = X@theta, if 'h' not numpy.ndarray it won't be used
 
     Return scalar
-    """
+    """ 
 
-    m = float(len(y[0]))
+    m = float(len(y)) # ins this case len(y) = len(y[:[0]])
 
     if (h is not None) and (type(h) is np.ndarray):
         if h.shape != y.shape :
@@ -28,20 +29,49 @@ def compute_cost(X, y, theta, h=None):
 
 def GD(X, y, theta, alfa, max_iter=10, tolerance=0.1, use_compute_cost = True):
     """
-    X is (m,n) vector.
+    X is (m,n)
     y is (m,1)
     theta is (n,1) 
 
     return theta(n,1), J[iter]
     """
-    m = float(len(y[0]))
+    m = float(len(y)) # ins this case len(y) = len(y[:[0]])
     if use_compute_cost: J = []
      
     for i in range(max_iter):
         h = X@theta
         if use_compute_cost: J.append(compute_cost(X,y,theta,h))
-              
+        
+        #check tolerance
+        if i>=1:
+            if abs(J[-1]-J[-2]) <= tolerance:
+                break
+
+
         theta -= (alfa/m)*X.T@(h-y)
 
     if use_compute_cost: return theta,J
     else: return theta
+
+def plot(theta, J, x):
+    """
+    theta is the line parameters
+    J is the cost funcion list
+    X = input data, with ones column
+    """
+    plot_x = np.sort(x[:,1])
+    plot_y = theta[0,0]+theta[1,0]*plot_x
+    plt.plot(plot_x, plot_y , '-', label='Line fit', linewidth=2, markersize=12, color='#FF0000')
+
+    plt.xlabel('Size')
+    plt.ylabel('Price')
+    plt.legend(loc='best')
+    plt.figure(0)
+
+    plt.plot(list(range(0,len(J))), J,'-', label=r'J($\theta$)')
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.legend(loc='best')
+    plt.figure(1)
+
+    plt.show()
